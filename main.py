a7f4617cc5c45a1fec35d22a8917d33a4b49a685
mattialosz <mattia.loszach@gmail.com>
2025-04-26 13:57:27 +0200

    Small optimizations & README.md

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def chat():
     print("\033[1;32m Personal Diabetes Assistant\033[0m")
     print("\033[90m powered by LLM & RAG\033[0m")
     print("\033[1;36m==============================\033[0m")
-    print("\033[90mType your question here (or type '/q', '/quit', 'exit' to quit):\033[0m")
+    print("\033[90mType your question here (or type '/q', '/quit', '/exit' to quit):\n\033[0m")
     while True:
         question = input(">>> ")
         if question.lower() in exit_kw:
             break
         if question.lower()[0] == '/':
-            print("\033[90mType your question here (or type '/q', '/quit', 'exit' to quit):\033[0m")
+            print("\033[90mType your question here (or type '/q', '/quit', '/exit' to quit):\033[0m")
             continue
         
         profiles = retriever.invoke(question) # Find relevant Vector DB entries
